fe12e888317f8de593da28b0eb2e958af0410d60
JermaineG310 <jgraham901@boyslatin.net>
2025-02-18 19:23:40 +0000

    i followed along with the reveiw for today.

---
+++ operator.py
@@ -0,0 +1,12 @@
+# Data manipulation
+
+# in python we have operators, that
+# allow us to change data
+
+# assigment operaterors 
+
+# comparison operateors
+
+# logical operateror
+
+# Arithamtic operator
---
+++ operator_1.py
@@ -0,0 +1,40 @@
+# Data manipulation
+
+# in python we have operators, that
+# allow us to change data
+
+# assigment operaterors 
+
+# comparison operateors
+
+# logical operateror
+
+# Arithamtic operater 
+bank account = [1000000]
+
+10<20 # is 10 less than 20 
+20.34 > = 0 # is 20.34 greater than or equal to 0 
+100 == 100 # is 100 the same as 100 
+100 != 10 # is 100 not the same as 10 
+
+
+# arthmatic operator allows us to peform math with - specifically with the integer and float data type
+
+
+10 + 10 # addition 
+12-2 # subtraction
+10*3 # multiplication 
+2/1 # division 
+
+# we use logical operators
+10*3=30
+100/4=25
+Name = Jermaine 
+
+
+Password = 123 
+SecurityQuestion = 'Franks'
+# and- used to comapre to conditions that are both true
+#not- used to compare conditons but will
+#output the opposite result- meaning if two things are true
+# we are goingg to get false
