4e700c160c7019a43f24576774c8d9bafa698520
JermaineG310 <jgraham901@boyslatin.net>
2025-02-21 19:29:40 +0000

    followed along with todays review

---
+++ Exercise.py
@@ -0,0 +1,28 @@
+# use one of the datacasting functions too fix the following code so that the print functions show strings. 
+# Create 2 varibles that contain the input function and then multiply and print thr result of the two varibles. 
+
+number=203 
+print(number)
+
+
+1. Commit and sync your work to your github repository. - DO NOT PUT THE LINK TO YOU CODESPACES HERE- IT MOST BE YOUR REPOSITIORY WEB LINK.
+2. Drop the link to your github repository in the following link below:
+[Async Activity Feb.21, 2025 Submission Link](http://forms.gle/Fhjj1X3mYnNnqZC57)
+
+## <b>Read through the prompt carefully, then answer the following quesitons</b>
+
+1. In your own words, describe what a python variable is? Please write your response in complete sentences.
+
+2. What is data type is stored in this variable?
+x = 1023.23
+
+3. which operator family would I use if I wanted to divide 2 numbers? Please provide your response as a string and
+then use the print function to output an example of 2 variables being divided in code.
+
+4. What datatype will the int() operator out put if I pass in the string "49"?
+
+5. Which built-in function would I use if I wanted to change an integer data type into a float data type
+
+6. Which operator would I use if I wanted to do determine if I have more than or fewer than credits to pass a certain class?
+
+7. Which operator would I use if I wanted to create a job search app and I wanted to take in user data like their name, their address, and their job title?
