e88cecd456fd2b8ea59dbc8a30c04eac71d3309f
JermaineG310 <jgraham901@boyslatin.net>
2025-02-24 18:22:20 +0000

    i followed along with todays note review and did what was needed of me.

--- operator.py
+++ operator.py
@@ -1,12 +1,42 @@
-# Data manipulation
+#1 
+ A python varible is ts a container for data types.
+ # name is the conatiner and the value "ian" is the data 
+ name= "jermaine"
 
-# in python we have operators, that
-# allow us to change data
 
-# assigment operaterors 
+#2
+# what data type is inside the varible?
+# This is a float data type. A float is a number with a decimal
+x= 102.3
 
-# comparison operateors
 
-# logical operateror
 
-# Arithamtic operator
+#3
+# what operator would i use to divide two varibles?
+"arthimitic operator"
+#use this print fuction to show the output of two varibles
+#being divided
+
+
+numberOne= 8
+numberTwo= 4
+
+print(numberOne / numberTwo)
+
+
+#4
+# what will happen if I pass the string 49 in the int() function
+print(int(49)) #will transform a integer into a float
+
+
+#5
+# what built in function would transform a integer into a float
+float(30) # 30.0
+
+creditNeedToPass = 100
+currentCredits = 80
+
+
+name = jermaine
+title = engineer 
+age = 'old' 
